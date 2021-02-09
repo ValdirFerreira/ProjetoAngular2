8f108253a482fdcc31201cf1c3e04972a8599597
Valdir <valdir.silva@fcamara.com.br>
2021-02-08 22:53:47 -0300

    primeiro pust

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Service } from '../app/Service/AppService';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,79 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ProjetoAngular';
+  tokenUsser: string = "";
+
+  constructor(public AppService: Service) {
+  }
+
+
+  ngOnInit() {
+
+    this.GerarToken();
+
+    this.Cadastrar();
+
+    this.GerarToken();
+
+  }
+
+  GerarToken() {
+    this.AppService.GerarToken().toPromise().then((res) => {
+      debugger
+      this.tokenUsser = res;
+    })
+  }
+
+  ListarProdutos() {
+    setTimeout(() => {
+      this.Listar();
+    }, 5000);
+  }
+
+Cadastrar()
+{
+  setTimeout(() => {
+    this.CadastrarProdutos();
+  }, 5000);
+  
+}
+
+
+  Listar() {
+    this.AppService.ListaProdutos(this.tokenUsser)
+      .toPromise()
+      .then((produtos) => {
+        debugger
+        var listaTeste = produtos;
+        debugger
+      })
+      .catch((err) => {
+
+        debugger
+        var erros = err;
+      });
+  }
+
+  CadastrarProdutos() {
+    debugger
+    var produto =
+    {
+      Id: 0,
+      Nome: "Teste",
+      Imagem: ""
+    };
+    debugger
+    this.AppService.InsereProduto(produto, this.tokenUsser)
+      .toPromise()
+      .then((okk) => {
+        debugger
+        var ok = okk;
+      })
+      .catch((err) => {
+        debugger
+        var erros = err;
+      });
+
+  }
+
 }
